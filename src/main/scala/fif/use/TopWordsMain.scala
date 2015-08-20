f45d6379b5c65b3c7f0e6847661b1c4a6c8b6683
Malcolm Greaves <greaves.malcolm@gmail.com>
2015-08-19 18:21:12 -0700

    wip demo

--- src/main/scala/fif/use/TopWordsMain.scala
+++ src/main/scala/fif/use/TopWordsMain.scala
@@ -1,13 +1,33 @@
 package fif.use
 
+import java.io.File
+
 import algebra.Semigroup
 import fif.Data
 
 import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
 import scala.language.higherKinds
 import scala.reflect.ClassTag
 
 object TopWordsMain extends App {
+
+  val dir = new File("./src/test/resources/")
+
+  val documents: Seq[TopWords.Document] =
+    dir
+      .listFiles().toSeq
+      .zipWithIndex
+      .map {
+        case (fi, index) =>
+          val text: Traversable[String] =
+            Source.fromFile(fi)
+              .getLines()
+              .flatMap(_.split(" "))
+              .toIndexedSeq
+
+          (index.toLong, text)
+      }
 
 
 }
